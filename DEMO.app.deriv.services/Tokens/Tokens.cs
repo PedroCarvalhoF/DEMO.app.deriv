0356e3a5224423649974199620674becb03070b4
PedroCarvalhoF <operacoes.pedro@gmail.com>
2024-11-27 11:31:56 -0300

    feat: estrutura inicial do projeto

---
+++ DEMO.app.deriv.services/Tokens/Tokens.cs
@@ -0,0 +1,30 @@
+﻿namespace DEMO.app.deriv.services.Tokens
+{
+    public static class Tokens
+    {
+        public static DefinicoesToken GetTokenLeitura()
+             => DefinicoesToken.Leitura("Demo_temp_token", "***********g6kX", Ambito.ApenasLeitura, "Nunca");
+    }
+
+    public class DefinicoesToken
+    {
+        public string Nome { get; private set; }
+        public string Token { get; private set; }
+        public Ambito Ambito { get; private set; }
+        public string UtimaAtualizacao { get; private set; }
+        DefinicoesToken(string nome, string token, Ambito ambito, string utimaAtualizacao)
+        {
+            Nome = nome;
+            Token = token;
+            Ambito = ambito;
+            UtimaAtualizacao = utimaAtualizacao;
+        }
+
+        public static DefinicoesToken Leitura(string nome, string token, Ambito ambito, string utimaAtualizacao)
+            => new DefinicoesToken(nome, token, ambito, utimaAtualizacao);
+    }
+    public enum Ambito
+    {
+        ApenasLeitura
+    }
+}
